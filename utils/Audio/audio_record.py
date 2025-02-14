2fec849efbaf594e35ecfded16ad5a7260315a96
lixinghe1999 <helixing99@gmail.com>
2025-02-14 15:25:26 +0800

    1

--- utils/Audio/audio_record.py
+++ utils/Audio/audio_record.py
@@ -19,7 +19,7 @@ def receive_audio(dataset_folder, device, fs=48000, duration=5, channels=2):
     '''
     receive by sounddevice and save the audio data
     '''
-    idx = get_device_index_by_name('device')
+    idx = get_device_index_by_name(device)
     print(f'Using device index: {idx}')
     # Set the parameters
     sd.default.device = idx
