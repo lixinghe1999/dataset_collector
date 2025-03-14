7e2ffb4b0d0b2dbe574a2a422b08cbf97dd82071
lixinghe1999 <helixing99@gmail.com>
2025-03-14 16:26:41 +0800

    still use writer

--- utils/IMU/bmi160.py
+++ utils/IMU/bmi160.py
@@ -79,20 +79,19 @@ def receive_imu(dataset_folder, sample_rate=1600, t=5, port=1):
     filename = os.path.join(dataset_folder, f'{datetime_str}.txt')
     writer = open(filename, 'w')
     imu = ''
-    imu = []
+    # imu = []
     t_start = time.time()
     # sensor.setIntDataReadyEnabled(True)
     while i < sample_rate * t:
         if sensor.getIntACCDataReadyStatus() and sensor.getIntGYRODataReadyStatus():
         # if sensor.getIntDataReadyEnabled():
             data = sensor.getMotion6() # gx, gy, gz, ax, ay, az
-            # imu += str(data[0]) + ' ' + str(data[1]) + ' ' + str(data[2]) + ' ' + str(data[3]) + ' ' \
-            #         + str(data[4]) + ' ' + str(data[5]) + ' ' + str(time.time() - t_start) + '\n'
-            imu.append(list(data) + [time.time() - t_start])
-            print(imu[-1])
+            imu += str(data[0]) + ' ' + str(data[1]) + ' ' + str(data[2]) + ' ' + str(data[3]) + ' ' \
+                    + str(data[4]) + ' ' + str(data[5]) + ' ' + str(time.time() - t_start) + '\n'
+            # imu.append(list(data) + [time.time() - t_start])
             i += 1
-    # writer.write(imu)
-    np.savetxt(filename, np.array(imu, dtype=np.int32))
+    writer.write(imu)
+    # np.savetxt(filename, np.array(imu, dtype=np.int32))
     real_sample_rate = sample_rate * t / (time.time() - t_start)
     print('IMU saved at:', filename)
     print('the real sample rate is:', real_sample_rate)
